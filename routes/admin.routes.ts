8c21369efefee787d0745aab995a3a6858d73ad1
Smil <me@smilt.dev>
2026-07-13 18:48:36 +0300

    feat: add admin stuff (non functional commit)

---
+++ routes/admin.routes.ts
@@ -0,0 +1,21 @@
+
+import { getPost, getPosts } from "@/utils/blog"
+import spaRender from "@/utils/spa"
+
+import { Router } from "express"
+const router = Router()
+
+router.get("/", async (req, res) => {
+    spaRender(req, res, "blog/index", "Blog", { posts: getPosts() })
+})
+
+router.get("/:slug", async (req, res) => {
+    const { slug } = req.params
+    
+    const post = getPost(slug)
+    if (!post) return res.status(404).render("404")
+
+    spaRender(req, res, "blog/view", post.title, { post })
+})
+
+export default router
